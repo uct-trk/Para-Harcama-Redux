32dd98febe21500f77f01f6fafc16b56a8678280
uğurcan türk <ugurcan.uut82@gmail.com>
2021-11-13 15:20:09 +0300

    redux kurulum, type belirleme

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "antd/dist/antd.css";
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
